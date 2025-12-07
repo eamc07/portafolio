be0aba1a25ad5441a3e7a321fecbcb7315106c88
Mendez <alexiscantillo07@gmail.com>
2025-12-06 21:00:51 -0400

    cambio de mensaje desde dev 2 inicio.php

--- inicio.php
+++ inicio.php
@@ -30,7 +30,7 @@ $proyectos = $obj_conexion->consuta("SELECT * FROM `proyectos`");
                 <div class="h-100 p-5 text-white bg-primary border rounded-3">
                     <h2>Bienvenido al Portafolio</h2>
                     <p>
-                        Hola como estas desde dev.
+                        Hola como estas desde dev2.
                     </p>
                     <button class="btn btn-outline-primary" type="button">
                         Mas info
